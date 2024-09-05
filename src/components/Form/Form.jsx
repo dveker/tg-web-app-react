ab036be826c9ba8c71b6481a3e7a95dcf82535e8
David Vekker <vekkerdavid1235@gmail.com>
2024-09-06 00:08:40 +0400

    first commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -21,7 +21,7 @@ const Form = () => {
         } else {
             tg.MainButton.show();
         }
-    }, [country, street])
+    }, [country, city])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
